4fffa34c23a56ac83c5009ab813c134a57cb180d
Ben Younes Ousama <benyounes.ousama@gmail.com>
2016-09-09 17:01:29 +0200

    fix: archi problem fixed using trait feature

--- generated/CurlTest.php
+++ generated/CurlTest.php
@@ -1,55 +1,9 @@
 <?php
 
 
-class CurlTest extends \PHPUnit_Framework_TestCase
+class CurlTest
 {
-    /**
-     * Current Class Name
-     *
-     * @var string
-     */
-    protected $className;
-
-
-
-
-    /**
-     * Take a screenshot
-     * @param string $filename
-     */
-    public function screenshot($filename) {
-        $filedata = $this->currentScreenshot();
-        file_put_contents('sceenshots/' . $this->getClassName() . '/' . $filename, $filedata);
-    }
-
-    /**
-     * Create a Directory
-     *
-     * @param $path
-     * @return bool
-     */
-    public function createDir($path)
-    {
-        if (!file_exists($path)) {
-            return mkdir($path);
-        }
-    }
-
-    /**
-     * @return string
-     */
-    public function getClassName()
-    {
-        return $this->className;
-    }
-
-    /**
-     * @param string $className
-     */
-    public function setClassName($className)
-    {
-        $this->className = $className;
-    }
+    use Utils;
 
     /**
      * @param $url
